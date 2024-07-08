c7accd70885a49bd3baec15835eec04a29d90f05
Samuel Leal (Valtech.com) <samuel.leal@valtech.com>
2024-07-08 17:35:36 -0300

    #1 feat: creates Header component
    - implements header with variant and responsiveness
    - creates Header story
    - implements media-query mixin
    - replaces css variables with sass variables

---
+++ src/features/Header/Header.d.ts
@@ -0,0 +1,14 @@
+export type THeader = {
+  homeLogo?: {
+    src: string;
+    alt: string;
+  };
+  pagesLogo?: {
+    src: string;
+    alt: string;
+  };
+  mobileLogo?: {
+    src: string;
+    alt: string;
+  };
+};
---
+++ src/features/Header/Header.mocks.ts
@@ -0,0 +1,14 @@
+export const data = {
+  homeLogo: {
+    src: "imgs/logo.svg",
+    alt: "Logo",
+  },
+  pagesLogo: {
+    src: "imgs/logo2.svg",
+    alt: "Logo",
+  },
+  mobileLogo: {
+    src: "imgs/logo3.svg",
+    alt: "Logo",
+  },
+};
