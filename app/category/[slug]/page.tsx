a275a81851d01f3187d03c560ddf5c9802055f54
Samuel Leal (Valtech.com) <samuel.leal@valtech.com>
2024-07-09 16:52:35 -0300

    #9 feat(jobs): Dynamic metadata on dynamic routes

--- app/category/[slug]/page.tsx
+++ app/category/[slug]/page.tsx
@@ -3,10 +3,25 @@ import { Header } from "@/src/features/Header";
 import { Metadata } from "next";
 import { JobsList } from "@/src/features/JobsList";
 
-export const metadata: Metadata = {
-  title: "Category title",
-  description: "Category description",
-};
+export async function generateMetadata({
+  params,
+}: CategoryPageProps): Promise<Metadata> {
+  const slug = params.slug;
+  const response = await fetch(process.env.URL + `/api/categories/${slug}`);
+  const { data } = await response.json();
+
+  if (data.length) {
+    return {
+      title: "Jeanne Dosse works on " + slug,
+      description: slug + " Jobs",
+    };
+  }
+
+  return {
+    title: "Category not found",
+    description: "Category not found",
+  };
+}
 
 export type CategoryPageProps = { params: { slug: string } };
 
--- app/job/[slug]/page.tsx
+++ app/job/[slug]/page.tsx
@@ -3,21 +3,34 @@ import { Metadata } from "next";
 import { Header } from "@/src/features/Header";
 import { JobPanel } from "@/src/features/JobPanel";
 
-export const metadata: Metadata = {
-  title: "Job title",
-  description: "Job description",
-};
+export async function generateMetadata({ params }: JobPageProps): Promise<Metadata> {
+  const slug = params.slug;
+  const response = await fetch(process.env.URL + `/api/jobs/${slug}`);
+  const { data } = await response.json();
+
+  if (data) {
+    return {
+      title: data.title,
+      description: data.details,
+    };
+  }
+
+  return {
+    title: 'Job not found',
+    description: 'Job not found',
+  }
+}
 
 export type JobPageProps = { params: { slug: string } };
 
-export default function Page({ params }: JobPageProps ) {
+export default function Page({ params }: JobPageProps) {
   const { pagesLogo, mobileLogo } = data;
 
   return (
     <>
       <Header pagesLogo={pagesLogo} mobileLogo={mobileLogo} />
       <main role="main">
-        <JobPanel slug={params.slug}/>
+        <JobPanel slug={params.slug} />
       </main>
     </>
   );
