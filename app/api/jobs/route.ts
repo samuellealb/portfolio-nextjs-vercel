4fc681d292c4f09e1d9d120a895eaf769a36839c
Samuel Leal (Valtech.com) <samuel.leal@valtech.com>
2024-07-17 12:37:25 -0300

    fix: fixed lint errors

--- app/api/jobs/route.ts
+++ app/api/jobs/route.ts
@@ -1,8 +1,8 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import { Job } from '@/src/lib/types';
 import { getJobs } from '@/src/lib/jobs';
 
-export async function GET(_request: NextRequest): Promise<NextResponse> {
+export async function GET(): Promise<NextResponse> {
   const jobsList: Job[] | undefined = await getJobs();
 
   if (!jobsList) {
