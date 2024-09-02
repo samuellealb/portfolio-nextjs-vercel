ba98bef7c4fee18d98cea6e00388a19897947bb0
Samuel Leal (Valtech.com) <samuel.leal@valtech.com>
2024-09-02 16:20:12 -0300

    #22 feat: basic slider style and config

--- src/components/GallerySlider/GallerySlider.tsx
+++ src/components/GallerySlider/GallerySlider.tsx
@@ -1,6 +1,5 @@
 import Glide from '@glidejs/glide';
 import '@glidejs/glide/dist/css/glide.core.min.css';
-// @import '@glidejs/glide/dist/css/glide.theme.min.css';
 import Image from 'next/image';
 import styles from './GallerySlider.module.scss';
 import { TGallerySlider } from './GallerySlider.d';
@@ -9,18 +8,30 @@ import { useEffect } from 'react';
 export const GallerySlider = ({ images }: TGallerySlider) => {
   useEffect(() => {
     if (!images) return;
-    new Glide('.glide').mount();
+    const sliderOptions = {
+      type: 'carousel' as const,
+      perView: 1,
+      focusAt: 'center',
+      keyboard: true,
+    };
+    new Glide('.glide', sliderOptions).mount();
   }, [images]);
 
   return (
-    <div className={styles.GallerySlider}>
+    <div className={styles.Gallery}>
       {images && (
-        <div className="glide">
-          <div className="glide__track" data-glide-el="track">
-            <ul className="glide__slides">
+        <div className={[`glide`, styles.GallerySlider].join(' ')}>
+          <div
+            className={[`glide__track`, styles.GalleryTrack].join(' ')}
+            data-glide-el="track"
+          >
+            <ul className={[`glide__slides`, styles.GallerySlides].join(' ')}>
               {images.map((image, index: number) => {
                 return (
-                  <li key={`image-slider-${index}`} className="glide__slide">
+                  <li
+                    key={`image-slider-${index}`}
+                    className={['glide__slide', styles.GallerySlide].join(' ')}
+                  >
                     <Image
                       src={image.url}
                       alt={image.title}
