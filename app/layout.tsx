a7cf053b8d1a01f093a69df7360d624e8cf1a0ef
Samuel Leal <samuel.leal@valtech.com>
2024-11-02 17:13:12 -0300

    feat: refactor BodyClassManager to a separate file and simplify layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
-'use client';
-
-import React, { useEffect, useContext } from 'react';
+import React from 'react';
 import '@/src/styles/index.scss';
 import { Base } from '@/src/layouts/Base';
-import { ModalProvider, ModalContext } from '@/src/context/ModalContext';
+import { ModalProvider } from '@/src/context/ModalContext';
+import { BodyClassManager } from '@/src/lib/bodyClassHandler';
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -21,18 +20,4 @@ export default function RootLayout({ children }: LayoutProps) {
       </body>
     </html>
   );
-}
-
-function BodyClassManager({ children }: { children: React.ReactNode }) {
-  const { modalOpen } = useContext(ModalContext);
-
-  useEffect(() => {
-    if (modalOpen) {
-      document.body.classList.add('modal-open');
-    } else {
-      document.body.classList.remove('modal-open');
-    }
-  }, [modalOpen]);
-
-  return <>{children}</>;
 }
