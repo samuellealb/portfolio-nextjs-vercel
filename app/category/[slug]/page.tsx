bfc4c87d96c3bc3c786dbe68e3120e8b73578501
Samuel Leal <samuel.leal@valtech.com>
2024-11-03 12:31:32 -0300

    fix: moved fetch bio data logic to page template

--- app/category/[slug]/page.tsx
+++ app/category/[slug]/page.tsx
@@ -2,7 +2,8 @@ import { Header } from '@/src/features/Header';
 import { NavBar } from '@/src/components/NavBar/NavBar';
 import { Metadata } from 'next';
 import { JobsList } from '@/src/features/JobsList';
-import { getCategory } from '@/src/lib/categories';
+import { getCategory, getCategories } from '@/src/lib/categories';
+import { getBio } from '@/src/lib/bio';
 import { getLogos } from '@/src/lib/logos';
 
 export async function generateMetadata({
@@ -27,11 +28,13 @@ export type CategoryPageProps = { params: { slug: string } };
 export default async function CategoryPage({ params }: CategoryPageProps) {
   const { headerListPage, headerMobile } = await getLogos();
   const categoryData = await getCategory(params.slug);
+  const categories = await getCategories();
+  const bioData = await getBio('3xXi5X2KBSsFJqnCNYNSuJ');
 
   return (
     <>
       <Header homeLogo={headerListPage} mobileLogo={headerMobile} />
-      <NavBar />
+      <NavBar categories={categories} bioTitle={bioData.title} />
       <main role="main">
         <JobsList {...categoryData} />
       </main>
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import { NavBar } from '@/src/components/NavBar/NavBar';
 import { JobsList } from '@/src/features/JobsList';
 import { getJobs } from '@/src/lib/jobs';
 import { getLogos } from '@/src/lib/logos';
+import { getCategories } from '@/src/lib/categories';
+import { getBio } from '@/src/lib/bio';
 import { TJob } from '@/src/lib/types';
 
 export const metadata: Metadata = {
@@ -14,11 +16,13 @@ export const metadata: Metadata = {
 export default async function Home() {
   const { headerDesktop, headerMobile } = await getLogos();
   const jobsList: TJob[] = await getJobs();
+  const categories = await getCategories();
+  const bioData = await getBio('3xXi5X2KBSsFJqnCNYNSuJ');
 
   return (
     <>
       <Header homeLogo={headerDesktop} mobileLogo={headerMobile} />
-      <NavBar />
+      <NavBar categories={categories} bioTitle={bioData.title} />
       <main role="main">
         <JobsList {...jobsList} />
       </main>
