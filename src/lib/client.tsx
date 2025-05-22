17833e8962e1ed40fa409af79fa5050570c42417
Samuel Leal (Valtech.com) <samuel.leal@valtech.com>
2025-05-22 14:36:32 -0300

    fix(#70): correct GraphQL URL environment variable reference

--- src/lib/client.tsx
+++ src/lib/client.tsx
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: `${process.env.CONTENTFUL_GRAPHQL_URL}/content/v1/spaces/${process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID}/environments/${process.env.NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT}`,
+  uri: `${process.env.NEXT_PUBLIC_CONTENTFUL_GRAPHQL_URL}/content/v1/spaces/${process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID}/environments/${process.env.NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT}`,
   cache: new InMemoryCache(),
   headers: {
     Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
