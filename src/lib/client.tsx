d3a3c8bdfa72ca2090d4cc23e23d145ca7eb9f5f
Samuel Leal (Valtech.com) <samuel.leal@valtech.com>
2025-05-22 07:48:33 -0300

    feat(#70): removed public prefix from private variables

--- src/lib/client.tsx
+++ src/lib/client.tsx
@@ -1,9 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: `${process.env.NEXT_PUBLIC_CONTENTFUL_GRAPHQL_URL}/content/v1/spaces/${process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID}/environments/${process.env.NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT}`,
+  uri: `${process.env.CONTENTFUL_GRAPHQL_URL}/content/v1/spaces/${process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID}/environments/${process.env.NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT}`,
   cache: new InMemoryCache(),
   headers: {
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN}`,
+    Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
   },
 });
